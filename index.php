49e2da47f1de496e63e12fa5ded81937f61cb6fa
Ast415 <bastienbories81@gmail.com>
2022-07-28 18:51:35 +0200

    passage de index html a php
    au cas ou je grade quelle que temps index.html en disbled

---
+++ index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil</title>
+    <link rel="stylesheet" href="css/global.css">
+    <link rel="stylesheet" href="css/font.css">
+</head>
+<body>
+    <!--Importation de l'en tete (header)-->
+    <?php include("enTete.html") ?>
+    <main>
+        <div class="CorpDePage">
+            <p>
+                Hello<br>
+                Hello<br>
+                Hello<br>
+                Hello<br>
+                Hello<br>
+                Hello<br>
+                Hello<br>
+                Hello<br>
+                Hello<br>
+                Hello<br>
+                Hello<br>
+                Hello<br>
+                Hello<br>
+                Hello<br>
+                Hello<br>
+                Hello<br>
+                Hello<br>
+                Hello<br>
+                Hello<br>
+                Hello<br>
+                Hello<br>
+                Hello<br>
+                Hello<br>
+                Hello<br>
+                Hello<br>
+                Hello<br>
+                Hello<br>
+                Hello<br>
+                Hello<br>
+                Hello<br>
+                Hello<br>
+                Hello<br>
+                Hello<br>
+                Hello<br>
+                Hello<br>
+                Hello<br>
+                Hello<br>
+                Hello<br>
+                Hello<br>
+                Hello<br>
+                Hello<br>
+                Hello<br>
+                Hello<br>
+                Hello<br>
+                Hello<br>
+                Hello<br>
+                Hello<br>
+                Hello<br>
+                Hello<br>
+                Hello<br>
+                Hello<br>
+                Hello<br>
+                Hello<br>
+                Hello<br>
+                Hello<br>
+                Hello<br>
+                Hello<br>
+                Hello<br>
+                Hello<br>
+                Hello<br>
+                Hello<br>
+                Hello<br>
+                Hello<br>
+            </p>
+        </div>
+    </main>
+    <!--Importation du pied de page (footer)-->
+    <?php include("piedDePage.html") ?>
+</body>
+</html>
