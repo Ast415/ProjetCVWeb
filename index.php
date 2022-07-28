5eb14569b1e0915b605f49a2778feb43d05dd406
Ast415 <bastienbories81@gmail.com>
2022-07-28 22:39:53 +0200

    Mise en forme et redaction

--- index.php
+++ index.php
@@ -9,78 +9,57 @@
     <link rel="stylesheet" href="css/font.css">
 </head>
 <body>
+
     <!--Importation de l'en tete (header)-->
     <?php include("enTete.html") ?>
+
     <main>
         <div class="CorpDePage">
+            <h2>Bienvenue</h2>
             <p>
-                Hello<br>
-                Hello<br>
-                Hello<br>
-                Hello<br>
-                Hello<br>
-                Hello<br>
-                Hello<br>
-                Hello<br>
-                Hello<br>
-                Hello<br>
-                Hello<br>
-                Hello<br>
-                Hello<br>
-                Hello<br>
-                Hello<br>
-                Hello<br>
-                Hello<br>
-                Hello<br>
-                Hello<br>
-                Hello<br>
-                Hello<br>
-                Hello<br>
-                Hello<br>
-                Hello<br>
-                Hello<br>
-                Hello<br>
-                Hello<br>
-                Hello<br>
-                Hello<br>
-                Hello<br>
-                Hello<br>
-                Hello<br>
-                Hello<br>
-                Hello<br>
-                Hello<br>
-                Hello<br>
-                Hello<br>
-                Hello<br>
-                Hello<br>
-                Hello<br>
-                Hello<br>
-                Hello<br>
-                Hello<br>
-                Hello<br>
-                Hello<br>
-                Hello<br>
-                Hello<br>
-                Hello<br>
-                Hello<br>
-                Hello<br>
-                Hello<br>
-                Hello<br>
-                Hello<br>
-                Hello<br>
-                Hello<br>
-                Hello<br>
-                Hello<br>
-                Hello<br>
-                Hello<br>
-                Hello<br>
-                Hello<br>
-                Hello<br>
-                Hello<br>
+                Vous vous trouver actuellement sur mon site web professionnelle, 
+                j'y regroupe tous mes projet personnelle, scolaire et professionnelle.
+                Mais pas que, comme je trouve qu'un CV est trop limitent en place j'ai 
+                désider de crée se site.<br>
+                Vous y retrouvrer donc aussi mon parcour scolaire et professionnelle, 
+                mes loisir, mes centre d'intérer et un presentation de moi même.
             </p>
+            <h2>Me contacter</h2>
+            <table class="AlignElement">
+                <tr>
+                    <td class="TdIcon">
+                        <img src="img/png/gmail.png" alt="" class="ContactIcon">
+                    </td>
+                    <td class="TdIcon">
+                        <img src="img/png/linkedin.png" alt="" class="ContactIcon">
+                        
+                    </td>
+                    <td class="TdIcon">
+                        <img src="img/png/telephone.png" alt="" class="ContactIcon">
+                    </td>
+                </tr>
+                <tr>
+                    <td class="TdIcon">
+                        Email : bastienbories81pro@gmail.com
+                    </td>
+                    <td class="TdIcon">
+                        Arrive bientot
+                    </td>
+                    <td class="TdIcon">
+                        N° 07 81 87 88 97
+                    </td>
+                </tr>
+            </table>
+            <h2>Télécharger mon CV</h2>
+            <p class="AlignElement">
+                <a href="doc/pdf/BORIES Bastien - CV.pdf"><img src="img/png/cv.png" alt="" class="ContactIcon" name="CV"></a>
+            </p>
+            
         </div>
     </main>
+
     <!--Importation du pied de page (footer)-->
     <?php include("piedDePage.html") ?>
+
 </body>
 </html>
