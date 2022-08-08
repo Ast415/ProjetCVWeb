7f36bdd4c1fb52a52794ae9285ccdfab70e0ef2b
Ast415 <bastienbories81@gmail.com>
2022-08-08 21:40:12 +0200

    nouvelle page d'erreur
    la nouvelle page est plus simple et plus en accord avec le site

---
+++ pageErr1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/global.css">
+    <link rel="stylesheet" href="css/font.css">
+</head>
+<body>
+    <?php include("enTete.html")?>
+    <div class="CorpDePage" id="err1CDP">
+        <h1 id="titreErr1">Désolée ...</h1>
+        <p id="txtErr1">La page n'est pas disponible pour l'instant,<br>revenez plus tard !</p>
+        <img id="gifErr1" src="src/gif/david-goodenough-joueur-du-grenier.gif">
+    </div>
+    <?php include("piedDePage.html") ?>
+</body>
+</html>
